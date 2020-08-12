89e801121a7b9eedf503d486ce3e48f63cad0774
Gianella Cortez <gianella.cortez@globant.com>
2020-08-11 19:54:34 -0700

    no message

--- js-variables.js
+++ js-variables.js
@@ -1,3 +1,18 @@
+/*
+Rules:
+Given the existing code below, can you write some code so that their values are switched around?
+    var a = "3";
+    var b = "8";
+
+So that the variable a holds the value "8".
+And the variable b holds the value "3".
+
+NOT change any of the existing code.
+NOT allowed to type any numbers
+NOT redeclare the variables a and b. 
+*/
+
+
 function test() {
     var a = "3";
     var b = "8";
