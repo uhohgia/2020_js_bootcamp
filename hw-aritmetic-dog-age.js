1e87c66e88f6f0aa1a9746d0dc0ea39d401145bd
Gianella Cortez <gianella.cortez@globant.com>
2020-08-12 19:46:50 -0700

    aritmetic js

---
+++ hw-aritmetic-dog-age.js
@@ -0,0 +1,21 @@
+/*
+dog age to humane age formula
+
+humanAge - (dogAGe - 2) x 4 + 21
+
+calculate human age of dog
+give result back to the user in an alert
+
+*/
+var dogAge = prompt("What is your dog age?");
+var humanAge = (dogAge-2)*4+21;
+
+alert("Yor dog human age is " + humanAge);
+
+
+// COURSE ANSWER
+var dogAge = prompt("What is your dog age?");
+var humanAge = ((dogAge-2) * 4) +  21;
+
+alert("Yor dog human age is " + humanAge);
+
---
+++ hw-aritmetic-increment-decrement-expressions.js
@@ -0,0 +1,33 @@
+//====== increment
+var x = 5;
+x++
+// x = 6
+console.log(x);
+
+//====== decrement
+var x = 5;
+x--
+// x = 4
+
+console.log(x);
+
+//====== increment by more than 1
+var x = 5;
+var y = 3;
+x+=y;
+// console.log(x+=y); // 8
+
+//====== decrement by more than 1
+var x = 5;
+var y = 3;
+x-=y;
+// console.log(x+=y); // 2
+
+
+//====== by more than 1
+x+=y;
+x-=y;
+x*=y;
+x/=y;
+
+
