e647514d127d38deb4be415a72478136cf2335c7
Gianella Cortez <gianella.cortez@globant.com>
2020-08-18 22:18:14 -0700

    life in weeks challenge instructions

---
+++ hw-life-in-weeks-challenge.js
@@ -0,0 +1,22 @@
+/*
+Life in weeks coding challenge
+
+Create a function that tells how many days, weeks and months 
+we have left until we live 90 years old.
+
+take current age as input
+console.log a meesage with the time left in this format:
+
+"You have X days, Y weeks, and Z months left.
+
+Where x, y and z are replaced with the calculated times.
+
+For this challenge, assume there are:
+365 days in a year
+52 weeks in a year
+12 months in a year
+*/
+
+function lifeInWeeks(age){
+	
+}
