8625e8884f2701e9a66ef24ecdb24617f9b397ee
Gianella Cortez <gianella.cortez@globant.com>
2020-08-22 21:29:59 -0700

    .includes arrays conditionals

---
+++ js-intermediate/hw-arrays-guestlist.js
@@ -0,0 +1,18 @@
+/*
+create a function that ask the name of a guest.
+compare the name to the list of guest
+if guest is in list return welcome message
+if guest is not return maybe later message
+*/
+
+
+function party(name){
+	var guestlist = ["angie", "jack", "pam", "james", "kris", "joe"];
+	if(guestlist.includes(name)){
+		console.log("welcome");
+	}else{
+		console.log("later");
+	}
+}
+
+party("paola");
