be63f336cf5679edb977c71c2a92be81cc0c8747
Gianella Cortez <gianella.cortez@globant.com>
2020-09-04 21:17:03 -0700

    for loop structure

---
+++ js-intermediate/hw-for-loop.js
@@ -0,0 +1,17 @@
+// for loop structure
+
+/*
+
+i=0; <- define a starting point
+i<2; <- define the end point
+i++; <-- change
+
+*/
+
+for(i = 0;  i < 2; i++){
+	// do something
+	console.log(i);
+}
+
+
+
