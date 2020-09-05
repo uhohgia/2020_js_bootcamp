085dae98ae8430a8e090b4c70f9d4a998ebb4c17
Gianella Cortez <gianella.cortez@globant.com>
2020-09-04 21:21:00 -0700

    while vs for

---
+++ js-intermediate/hw-while-vs-for-loops.js
@@ -0,0 +1,22 @@
+/*
+While loop vs For Loop
+*/
+
+while(/*something is true*/){
+	// do something
+}
+
+/*
+While loops check for a state
+Checks if statement is true
+*/
+
+for(i=0; i<2; i++){
+	// do something
+}
+
+/*
+For loops iterate
+Runs the code x number of times
+Defines how many times the code should be run
+*/
