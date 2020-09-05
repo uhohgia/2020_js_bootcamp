38f69bb96d667065f1d58ca5a255eb0492a28298
Gianella Cortez <gianella.cortez@globant.com>
2020-09-04 20:55:52 -0700

    99 bottles of beer exercise

---
+++ js-intermediate/hw-whileLoop-99bottles.js
@@ -0,0 +1,22 @@
+// 99 bottles of beer
+
+/*
+Use a while loop and print on console the following lyrics.
+
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+
+decrement value to
+
+No more bottles of beer on the wall, no more bottles of beer.
+Go the store and buy some more, 99 bottles of beer on the wall.
+*/
+
+// declare initial value
+var bottles = 99;
+
+while(bottles >= 0) /* do something if statement is true*/{
+	console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer."); 
+	bottles--;
+	console.log("Take one down and pass it around, " + bottles + " bottles of beer on the wall.");
+}
