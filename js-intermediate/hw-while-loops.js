3e2e407e2c4941c4a7991c5a3c444c520379208f
Gianella Cortez <gianella.cortez@globant.com>
2020-09-04 20:26:28 -0700

    while loop structure

---
+++ js-intermediate/hw-while-loops.js
@@ -0,0 +1,9 @@
+// while loop structure
+
+// declare initial value
+var i = 1;
+
+while(i<2) /* do something if statement is true*/{ 
+	console.log(i); // do something
+	i++; // increment + 1 so next time loop runs and evaluates the condition again, statement will be false
+}
