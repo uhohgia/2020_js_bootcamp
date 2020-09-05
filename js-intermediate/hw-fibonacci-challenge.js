ee55ea2056078734c144af6fd6803b29cf8244a9
Gianella Cortez <gianella.cortez@globant.com>
2020-09-04 21:41:48 -0700

    fibonacci challenge

---
+++ js-intermediate/hw-fibonacci-challenge.js
@@ -0,0 +1,39 @@
+/*Fibonacci challenge*/
+
+/*
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+Where every number is the sum of the two previous ones. 
+
+e.g. 0, 1, 1, 2, 3, 5 comes from
+0 + 1 = 1
+1 + 1 = 2
+1 + 2 = 3
+2 + 3 = 5
+
+Create a function where you can call it by writing the code:
+fibonacciGenerator (n)
+where "n" is the number of items in the sequence
+
+So I should be able to call:
+fibonacciGenerator(3)
+and get
+[0,1,1]
+as the output
+
+IMPORTANT: The solution checker is expecting an array  as the correct output.
+The result should be return from the function as an ouput
+First 2 numbers of the sequence mus be 0 and 1.
+Also, if you decide to create a for  loop, make sure you explicitly specify var i = 0
+example: for (var i = 0; i < 10; i ++)
+
+*/
+
+var output = [];
+function fibonacciGenerator(n) {
+	for (var i = 0 ; i >= 0; i++){
+		
+	}
+	console.log(output);
+}
+fibonacciGenerator(1);
+
