d91519d72a50fd806ce5fe77df73d167349a317f
Gianella Cortez <gianella.cortez@globant.com>
2020-09-27 21:17:13 -0700

    fibonacci generator challenge

--- js-intermediate/hw-fibonacci-challenge.js
+++ js-intermediate/hw-fibonacci-challenge.js
@@ -29,11 +29,20 @@ example: for (var i = 0; i < 10; i ++)
 
 */
 
-var output = [];
 function fibonacciGenerator(n) {
-
-	
+	var output = []; // empty array
+	if(n === 1){
+		output = [0];
+	}else if(n === 2){
+		ouput = [0,1];
+	}else{
+		output = [0,1];
+		for(var i = 2; i < n; i++){
+			output.push(output[output.length -2] + output[output.length -1]);
+		}
+	}
+	return output;
 	console.log(output);
 }
-fibonacciGenerator(1);
 
+fibonacciGenerator(2);
